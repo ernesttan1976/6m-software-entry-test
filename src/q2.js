aed6d7245f8dc326fb1e38689944cbcc11a3862f
ernesttan1976 <65232389+ernesttan1976@users.noreply.github.com>
2022-12-19 19:53:26 +0800

    Update q2.js

--- src/q2.js
+++ src/q2.js
@@ -11,16 +11,26 @@
 
 
 // Task 1: Add code here
+var patientNames=[];
 
 function addPatient(patientName){
     // Task 2: Add code here
+    patientNames.push(patientName);
 }
 
 // Task 3: Add code here
-
+function listPatient(){
+    var text="[";
+    for (var i=0;i<patientNames.length;i++)
+    {
+        text = text + "'" + patientNames[i] + "',"
+    }
+    text=text.substring(0,text.length-1)+"]";
+    return text;
+}
 
 addPatient("John");
 addPatient("Mary");
 addPatient("Mark");
 
-listPatient(); // This should list ["John", "Mary", "Mark"]
+console.log(listPatient()); // This should list ["John", "Mary", "Mark"]
